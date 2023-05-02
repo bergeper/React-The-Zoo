32a332cdef561cd71fd6dcc13978b1c352ea0647
Per Berge <per.berge93@gmail.com>
2023-05-02 10:19:20 +0200

    (feat): navbar now works

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Animals } from './pages/Animals';
+import { ViewAnimal } from './pages/ViewAnimal';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/animals',
+    element: <Animals></Animals>,
+  },
+  {
+    path: '/animals/:id',
+    element: <ViewAnimal></ViewAnimal>,
+  },
+]);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
