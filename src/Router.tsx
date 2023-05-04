e2a4e94663fab44be170851a2ccdea5be46fe122
Per Berge <per.berge93@gmail.com>
2023-05-04 14:56:05 +0200

    (refactor): moved routes to own file.
    Routes are new using children

---
+++ src/Router.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
+import { Animals } from './pages/Animals/Animals';
+import { DisplayAnimal } from './pages/DisplayAnimal/DisplayAnimal';
+import { Error } from './pages/Error/Error';
+import { Layout } from './components/Layout/Layout';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        errorElement: <Error></Error>,
+      },
+      {
+        path: '/animals',
+        element: <Animals></Animals>,
+      },
+      {
+        path: '/animals/:id',
+        element: <DisplayAnimal></DisplayAnimal>,
+      },
+    ],
+  },
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.scss';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Animals } from './pages/Animals';
-import { DisplayAnimal } from './pages/DisplayAnimal';
-import { Error } from './pages/Error';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Router';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home></Home>,
-    errorElement: <Error></Error>,
-  },
-  {
-    path: '/animals',
-    element: <Animals></Animals>,
-  },
-  {
-    path: '/animals/:id',
-    element: <DisplayAnimal></DisplayAnimal>,
-  },
-]);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
