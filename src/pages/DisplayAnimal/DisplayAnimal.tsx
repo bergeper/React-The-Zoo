ebebb0f9ecc015a2f3e90df2a77417c10f847e8c
Per Berge <per.berge93@gmail.com>
2023-05-08 14:49:31 +0200

    (refactor): was not using

--- src/pages/DisplayAnimal/DisplayAnimal.tsx
+++ src/pages/DisplayAnimal/DisplayAnimal.tsx
@@ -56,7 +56,6 @@ export const DisplayAnimal = () => {
             >
               Mata
             </button>
-            <i className='fa-solid fa-salad'></i>
           </article>
         </>
       ) : (
