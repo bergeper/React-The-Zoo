0465b0493aeb039b65f211a2ffc033a38da921c9
Per Berge <per.berge93@gmail.com>
2023-05-08 14:49:43 +0200

    (feat): link to homepage if error

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export const Error = () => {
   return (
     <>
       <h2>Oj då! Nu har något blivit tokigt</h2>
       <h4>Kontrollera adressen och försök igen</h4>
+      <Link to='/'>Tillbaka till startsidan</Link>
     </>
   );
 };
