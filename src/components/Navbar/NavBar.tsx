4e26534419225ba44e52b2be702a36f3745286b1
Per Berge <per.berge93@gmail.com>
2023-05-08 10:05:50 +0200

    (fix): title will now send you to homepage

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -4,7 +4,9 @@ import './Navbar.scss';
 export const Navbar = () => {
   return (
     <nav className='nav'>
-      <h3 className='nav__title'>The Zoo</h3>
+      <Link className='nav__menu--link' to='/'>
+        <h3 className='nav__title'>The Zoo</h3>
+      </Link>
       <ul className='nav__menu'>
         <li className='nav__menu--link'>
           <Link className='nav__menu--link' to='/'>
