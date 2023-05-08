a66ae834350f2ce4e70ca43f84b2406bfd2e7666
Per Berge <per.berge93@gmail.com>
2023-05-08 14:31:46 +0200

    (refactor): link will now be around logo and title

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -4,20 +4,17 @@ import './Navbar.scss';
 export const Navbar = () => {
   return (
     <nav className='nav'>
-      <Link className='nav__menu--link' to='/'>
-        <h3 className='nav__title'>The Zoo</h3>
+      <Link className='nav__home' to='/'>
+        <i className='fa-solid fa-hippo nav__home--logo'></i>
+        <h3 className='nav__home--title'>The Zoo</h3>
       </Link>
       <ul className='nav__menu'>
-        <li className='nav__menu--link'>
-          <Link className='nav__menu--link' to='/'>
-            Hem
-          </Link>
-        </li>
-        <li className='nav__menu--link'>
-          <Link className='nav__menu--link' to='/animals'>
-            Djuren
-          </Link>
-        </li>
+        <Link className='nav__menu--link' to='/'>
+          <li className='nav__menu--item'>Hem</li>
+        </Link>
+        <Link className='nav__menu--link' to='/animals'>
+          <li className='nav__menu--item'>Djuren</li>
+        </Link>
       </ul>
     </nav>
   );
